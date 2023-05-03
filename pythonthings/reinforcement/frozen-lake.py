6f51dda07353a376baf47ac8eb8f53bd8a27af9d
Liam Laverty <liamlaverty@gmail.com>
2023-05-03 15:54:21 +0100

    shows graphics for frozen lake

--- pythonthings/reinforcement/frozen-lake.py
+++ pythonthings/reinforcement/frozen-lake.py
@@ -1,14 +1,23 @@
 import gym
-
-env = gym.make('FrozenLake-v1', render_mode='rgb_array')
+env = gym.make('FrozenLake-v1', render_mode='human')
 
 
 n_state = env.observation_space.n
-print(n_state)
+print(f'n_state: {n_state}')
 
 n_action = env.action_space.n
-print(n_action)
+print(f'n_action: {n_action}')
 
 env.reset()
 
 env.render()
+
+discard, new_state, reward, is_done, info = env.step(2)
+print(f'new_state: {new_state}')
+print(f'reward: {reward}')
+print(f'info: {info}')
+
+discard, new_state, reward, is_done, info = env.step(3)
+print(f'new_state: {new_state}')
+print(f'reward: {reward}')
+print(f'info: {info}')
